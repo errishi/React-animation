8b4e4e7e3bea5bda5f0fa77d2cfd543716487d1c
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-10-06 18:04:38 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Agence from './pages/Agence';
 
 const App = () => {
   return (
-    <div className='text-6xl text-white'>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/agence' element={<Agence/>} />
+      </Routes>
+    </>
   )
 }
 
