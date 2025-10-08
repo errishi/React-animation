c1847f5456fbcdfec314df3bbce2664627815a6d
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-10-08 21:27:26 +0530

    project page animation

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { navImg } from '../../assets/assets';
 
 const ProjectCard = ({image1, image2}) => {
   return (
-    <div className='w-full h-[500px] mb-4 gap-4 flex'>
+    <>
       <div className='w-1/2 group transition-all relative cursor-pointer hover:rounded-[70px] overflow-hidden h-full'>
         <img className='w-full h-full object-cover' src={image1} alt="image" />
         <div className='opacity-0 group-hover:opacity-100 transition-all absolute flex items-center justify-center top-0 left-0 h-full w-full bg-black/10'>
@@ -16,7 +16,7 @@ const ProjectCard = ({image1, image2}) => {
           <h2 className='uppercase text-4xl font-[font1] border-2 px-4 pt-1.5 text-white border-white rounded-full'>View Project</h2>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ProjectCard from '../components/projects/ProjectCard';
 import { navImg, thumbnail } from '../assets/assets';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 const Projects = () => {
   const projects = [{
@@ -20,14 +23,36 @@ const Projects = () => {
     image2:thumbnail.thumbnail_img_2
   }];
 
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function(){
+    gsap.from('.hero', {
+      height:"40px",
+      stagger: {
+        amount: 0.4
+      },  
+      scrollTrigger: {
+        trigger: '.lol',
+        markers: true,
+        start: "top 100%",
+        end: "top -150%",
+        scrub: true
+      }
+    })
+  });
+
   return (
     <div className='p-2'>
       <div className='pt-[40vh]'>
         <h2 className='font-[font2] text-[10.5vw] uppercase'>Projects</h2>
       </div>
-      <div className='-mt-10'>
-        {projects.map(function(elem){
-          return <ProjectCard image1={elem.image1} image2={elem.image2} />
+      <div className='-mt-10 lol'>
+        {projects.map(function(elem, idx){
+          return (
+            <div key={idx} className='hero w-full h-[300px] mb-4 gap-4 flex'>
+              <ProjectCard image1={elem.image1} image2={elem.image2} />
+            </div>
+          ) 
         })}
       </div>
     </div>
