7a7feb2e44193991709d16309e28649530f94601
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-10-09 14:23:47 +0530

    agency page responsive

--- src/context/NavContext.jsx
+++ src/context/NavContext.jsx
@@ -1,14 +1,28 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 
 export const NavBarContext = createContext();
+export const NavBarColorContext = createContext();
 
 const NavContext = ({children}) => {
     const [navOpen, setNavOpen] = useState(false);
+    const [navColor, setNavColor] = useState("white");
+
+    const locate = useLocation().pathname;
+    useEffect(()=>{
+      if(locate == "/projects" || locate == "/agency"){
+        setNavColor("black");
+      }else{
+        setNavColor("white");
+      }
+    }, [locate]);
 
   return (
     <div>
         <NavBarContext.Provider value={[navOpen, setNavOpen]}>
+          <NavBarColorContext.Provider value={[navColor, setNavColor]} >
             {children}
+          </NavBarColorContext.Provider>
         </NavBarContext.Provider>
     </div>
   )
